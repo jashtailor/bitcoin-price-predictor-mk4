400e2eccb1202685cd53a058aee9fa557e41e081
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-05-12 23:00:45 +0530

    Update mk4.py

--- mk4.py
+++ mk4.py
@@ -20,7 +20,8 @@ from datetime import date
 import streamlit as st
 
 st.write("""
-## Please be patient this could take a while
+# Staver
+## Your one-stop solution for prediction and forecasting of Bitcoin 
 """)
 
 
@@ -31,6 +32,10 @@ st.write("""
 
 def time_series():
    
+     st.write("""
+     ## Please be patient this could take a while
+     """)
+     
      # Time Series Forecasting using FB-Prophet 
 
      st.write("""
@@ -83,12 +88,13 @@ def time_series():
 
      st.write(a['Date'], '\n', a['Lower limit of Prediction'], '\n', a['Upper limit of Prediction'], '\n', a['Prediction'])
 
-
-
-
-
-
 def reddit_SA():
+   
+   st.write("""
+   ## Please be patient this could take a while
+   """)
+     
+   
    # Reddit Sentiment Analysis 
 
    st.write("""
@@ -193,87 +199,94 @@ def reddit_SA():
    fig = px.bar(reddit_sent, x='Sentiment', y='Number of Posts', title='Reddit Sentiment Analysis')
    st.plotly_chart(fig)
 
+
+def twitter_SA():
+   
+   st.write("""
+   ## Please be patient this could take a while
+   """)
+     
+   
+   # Twitter Sentiment Analysis 
+
+   st.write("""
+   # Twitter Sentiment Analysis
+   """)
+
+   # twitter credentials
+   consumer_key= 'uLPC3KfMtGFcEeq4CxEOohZeg'
+   consumer_secret= 'tywsJRvcr2zz5ICg7bkadbSIIjhGFmAlOLjJECjPqMfaRuwc1T'
+   access_token= '1300465599823314944-VkC6tWnEUrbxTZ1wYpWIxbc8LQCPNL'
+   access_token_secret= 'DDiF0cmidxoQlT2rgEUCGkP4E2DI8PBwz6WMS5QL51zOG'
+   auth = tw.OAuthHandler(consumer_key, consumer_secret)
+   auth.set_access_token(access_token, access_token_secret)
+   api = tw.API(auth, wait_on_rate_limit=True)
+
+   search_words = ['crypto', 'bitcoin']
+   date_since = '2021-04-20'
+   tweet_text = []
+   date_time = []
+   location = []
+
+   # extracting tweet text, datetime and location
+   for words in search_words:
+     tweets = tw.Cursor(api.search,
+                 q=search_words,
+                 lang="en",
+                 since=date_since).items(1000)
+     for tweet in tweets:
+       str1 = tweet.text
+       str2 = tweet.created_at
+       str3 = tweet.user.location
+       tweet_text.append(str1)
+       date_time.append(str2)
+       location.append(str3)
+
+   df_twitter = pd.DataFrame()
+   df_twitter['Tweets'] = tweet_text
+   df_twitter['Created at'] = date_time
+   df_twitter['Location'] = location
+
+   sia = SIA()
+   results = []
+   for line in tweet_text:
+     pol_score = sia.polarity_scores(line)
+     pol_score['Tweets'] = line
+     results.append(pol_score)
+
+   df_twitter_nlp = pd.DataFrame(results)
+
+   # compound is taken as the deciding factor is classifying the sentiment
+
+   # positive
+   df_twitter_nlp.loc[df_twitter_nlp['compound'] > 0, 'Sentiment'] = '1'
+
+   # negative
+   df_twitter_nlp.loc[df_twitter_nlp['compound'] < 0, 'Sentiment'] = '-1'
+
+   # neutral
+   df_twitter_nlp.loc[df_twitter_nlp['compound'] == 0.0, 'Sentiment'] = '0'
+
+   df_twitter_nlp['Created at'] = date_time
+   df_twitter_nlp['Location'] = location
+
+   df_twitter_groupby = df_twitter_nlp.groupby('Sentiment').count()
+   lst2 = df_twitter_groupby['Tweets']
+   dict2 = {'Sentiment': sentiment_lst, 'Number of Tweets': lst2}
+   twitter_sent = pd.DataFrame(dict2)
+   fig = px.bar(twitter_sent, x='Sentiment', y='Number of Tweets', title='Twitter Sentiment Analysis')
+   st.plotly_chart(fig)
+
 option = st.selectbox(
-     'How would you like to be contacted?',
-     ('None', 'Reddit Sentiment Analysis', 'Time series', 'Home phone', 'Mobile phone'))
+     'Please select your desired algorithm',
+     ('None', 'Reddit Sentiment Analysis', 'Twitter Sentiment Analysis', 'Time series'))
 
 st.write('You selected:', option)
 if option == 'Time series':
    time_series()
 elif option == 'Reddit Sentiment Analysis':
    reddit_SA()
-   
-'''
-# Twitter Sentiment Analysis 
-
-st.write("""
-# Twitter Sentiment Analysis
-""")
-
-# twitter credentials
-consumer_key= 'uLPC3KfMtGFcEeq4CxEOohZeg'
-consumer_secret= 'tywsJRvcr2zz5ICg7bkadbSIIjhGFmAlOLjJECjPqMfaRuwc1T'
-access_token= '1300465599823314944-VkC6tWnEUrbxTZ1wYpWIxbc8LQCPNL'
-access_token_secret= 'DDiF0cmidxoQlT2rgEUCGkP4E2DI8PBwz6WMS5QL51zOG'
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
-
-search_words = ['crypto', 'bitcoin']
-date_since = '2021-04-20'
-tweet_text = []
-date_time = []
-location = []
-
-# extracting tweet text, datetime and location
-for words in search_words:
-  tweets = tw.Cursor(api.search,
-              q=search_words,
-              lang="en",
-              since=date_since).items(1000)
-  for tweet in tweets:
-    str1 = tweet.text
-    str2 = tweet.created_at
-    str3 = tweet.user.location
-    tweet_text.append(str1)
-    date_time.append(str2)
-    location.append(str3)
-
-df_twitter = pd.DataFrame()
-df_twitter['Tweets'] = tweet_text
-df_twitter['Created at'] = date_time
-df_twitter['Location'] = location
-
-sia = SIA()
-results = []
-for line in tweet_text:
-  pol_score = sia.polarity_scores(line)
-  pol_score['Tweets'] = line
-  results.append(pol_score)
-
-df_twitter_nlp = pd.DataFrame(results)
-
-# compound is taken as the deciding factor is classifying the sentiment
-
-# positive
-df_twitter_nlp.loc[df_twitter_nlp['compound'] > 0, 'Sentiment'] = '1'
-
-# negative
-df_twitter_nlp.loc[df_twitter_nlp['compound'] < 0, 'Sentiment'] = '-1'
-
-# neutral
-df_twitter_nlp.loc[df_twitter_nlp['compound'] == 0.0, 'Sentiment'] = '0'
-
-df_twitter_nlp['Created at'] = date_time
-df_twitter_nlp['Location'] = location
-
-df_twitter_groupby = df_twitter_nlp.groupby('Sentiment').count()
-lst2 = df_twitter_groupby['Tweets']
-dict2 = {'Sentiment': sentiment_lst, 'Number of Tweets': lst2}
-twitter_sent = pd.DataFrame(dict2)
-fig = px.bar(twitter_sent, x='Sentiment', y='Number of Tweets', title='Twitter Sentiment Analysis')
-st.plotly_chart(fig)
-'''
-
+elif option == 'Twitter Sentiment Analysis':
+   twitter_SA()
 
 
