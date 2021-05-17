04b62a10eaa948adc8675cf0e069b699d7481fe6
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-05-17 11:18:16 +0530

    Update mk4.py

--- mk4.py
+++ mk4.py
@@ -56,7 +56,7 @@ def time_series():
      df = tickerData.history(period='1d', start='2010-10-08', end=today)
      df.reset_index(inplace=True)
      
-     st.text("""
+     st.write("""
      Ticker chart of BTC-INR
      """)
      fig = go.Figure(data=go.Ohlc(x=df['Date'],
