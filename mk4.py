54b9e0250bfd17ee75367ca36b2de54bdaf6ae61
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-08-05 11:43:20 +0530

    Update mk4.py

--- mk4.py
+++ mk4.py
@@ -158,7 +158,7 @@ def sentiment_analysis():
      tweets = tw.Cursor(api.search,
                  q=search_words,
                  lang="en",
-                 since=date_since).items(500)
+                 since=date_since).items(100)
      for tweet in tweets:
        str1 = tweet.text
        str2 = tweet.created_at
